866da994031e74975a619783b1f22f50562db56d
Josh-Clemons <110069447+Josh-Clemons@users.noreply.github.com>
2022-11-23 06:39:25 -0600

    axios GET setup on app.js

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import './App.css';
+import Axios from 'axios';
+import {useState, useEffect} from 'react';
+
 
 function App() {
+
+  const [galleryItems, setGalleryItems] = useState([]);
+
+  useEffect( () => {getGallery()}, []) // check this syntax, tried it from memory
+
+  const getGallery = ()=> {Axios.get('/gallery').then((response) => {
+    setGalleryItems(response.data);
+  })};
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
+        <p>Gallery Items: {JSON.stringify(galleryItems)}</p>
         <p>Gallery goes here</p>
         <img src="images/goat_small.jpg"/>
       </div>
