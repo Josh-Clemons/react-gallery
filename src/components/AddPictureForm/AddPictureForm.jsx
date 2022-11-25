dba752df64e2b6d9cffbea10ad1512d5d8319510
Josh-Clemons <110069447+Josh-Clemons@users.noreply.github.com>
2022-11-25 14:00:50 -0600

    basic css for add pic form completed

--- src/components/AddPictureForm/AddPictureForm.jsx
+++ src/components/AddPictureForm/AddPictureForm.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import './AddPictureForm.css';
 
 function AddPictureForm () {
 
@@ -15,20 +16,20 @@ function AddPictureForm () {
     return (
         <div className="addPictureDiv">
             {(viewForm) ? 
-                <>
-                <h3>Add a picture:</h3>
-                <form className="inputForm" onSubmit={handleSubmit}>
-                    <label htmlFor="urlInput">URL: </label>
-                    <input id="urlInput" type="text" placeholder="paste URL here" onChange={(e)=>{setUrlValue(e.target.value)}}/>
-                    <br/>
-                    <label htmlFor="descriptionInput">Description: </label>
-                    <textarea id="descriptionInput" type="text" placeholder="brief description of image" cols={30} rows={3} onChange={(e)=>{setDescriptionValue(e.target.value)}}/>
-                    <br/>
-                    <button type="submit">Add Picture</button>
-                    <button onClick={() => setViewForm(!viewForm)}>Cancel</button>
-                </form> 
-                </>
-                : <h3 onClick={() => setViewForm(!viewForm)}>Click here to add your own picture!</h3>
+                <div className="addPictureTrue">
+                    <h3>Add a picture:</h3>
+                    <form className="inputForm" onSubmit={handleSubmit}>
+                        <label htmlFor="urlInput">URL: </label>
+                        <input id="urlInput" type="text" placeholder="paste URL here" onChange={(e)=>{setUrlValue(e.target.value)}}/>
+                        <br/>
+                        <label id="descriptionLabel" htmlFor="descriptionInput">Description: </label>
+                        <textarea id="descriptionInput" type="text" placeholder="brief description of image" cols={22} rows={3} onChange={(e)=>{setDescriptionValue(e.target.value)}}/>
+                        <br/>
+                        <button type="submit">Add Picture</button>
+                        <button onClick={() => setViewForm(!viewForm)}>Cancel</button>
+                    </form> 
+                </div>
+                : <h3 className="clickMe" onClick={() => setViewForm(!viewForm)}>Click here to add your own picture!</h3>
             }
             
         </div>
