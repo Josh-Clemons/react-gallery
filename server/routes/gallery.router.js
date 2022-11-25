a761465003c3af60b7ec6a3c575fdeb2f6d84a51
Josh-Clemons <110069447+Josh-Clemons@users.noreply.github.com>
2022-11-25 11:54:10 -0600

    put and get APIs linked

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -1,30 +1,31 @@
+const { query } = require('express');
 const express = require('express');
 const router = express.Router();
-const galleryItems = require('../modules/gallery.data');
 const pool = require('../modules/pool')
-
-// DO NOT MODIFY THIS FILE FOR BASE MODE
 
 // PUT Route
 router.put('/like/:id', (req, res) => {
-    console.log(req.params);
+    const queryText = 'UPDATE pictures SET likes = likes + 1 WHERE id = $1'
     const galleryId = req.params.id;
-    for(const galleryItem of galleryItems) {
-        if(galleryItem.id == galleryId) {
-            galleryItem.likes += 1;
-        }
-    }
-    res.sendStatus(200);
+
+    pool.query(queryText, [galleryId]).then((results) => {
+        console.log('success in like increment put');
+        res.sendStatus(200);
+    }).catch(error => {
+        alert('error PUTting to DB', error);
+    });
+
 }); // END PUT Route
 
 // GET Route
 router.get('/', (req, res) => {
-    const queryText = `SELECT * FROM pictures;`;
+    const queryText = `SELECT * FROM pictures ORDER BY id;`;
 
     pool.query(queryText).then((results) => {
         res.send(results.rows);
     }).catch((error) => {
         alert('error GETting from DB', error);
+        res.sendStatus(500);
     })
     
 }); // END GET Route
